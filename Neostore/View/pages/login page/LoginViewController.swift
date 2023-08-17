751bbd791838b7ef53b9195e3dc9b2143ae1f911
saad <saadneosoft@gmail.com>
2023-08-17 16:31:25 +0530

    basic

---
+++ Neostore/View/pages/login page/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Neostore
+//
+//  Created by Neosoft on 17/08/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
