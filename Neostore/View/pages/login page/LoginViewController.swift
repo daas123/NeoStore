d0f9cebc54bf7f5c69d7a8b08428cc34d63a175d
saad <saadneosoft@gmail.com>
2023-08-17 17:19:40 +0530

    login page design done

--- Neostore/View/pages/login page/LoginViewController.swift
+++ Neostore/View/pages/login page/LoginViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var personDetailsview: [UIView]!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        for userview in personDetailsview{
+            userview.layer.borderColor = UIColor.white.cgColor
+            userview.layer.cornerRadius = 1.0
+            
+        }
         // Do any additional setup after loading the view.
     }
 
