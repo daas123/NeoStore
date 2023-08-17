7c86b6cbf0de06110ff902ae250bb53048458f93
saad <saadneosoft@gmail.com>
2023-08-17 19:32:13 +0530

    little bit functionalities done

--- Neostore/View/pages/Register page/RegisterViewController.swift
+++ Neostore/View/pages/Register page/RegisterViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 class RegisterViewController: UIViewController {
 
     
+    @IBOutlet var registerDetailsView: [UIView]!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for registerViews in registerDetailsView{
+            registerViews.layer.borderWidth = 1
+            registerViews.layer.borderColor = UIColor.white.cgColor
+        }
     }
 
 
